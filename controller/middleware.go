ac8422797b8617ed19f8679c6da3cc0992858432
Kazuki Onishi <41312202+0n1shi@users.noreply.github.com>
2022-01-06 22:59:26 +0900

    :loud_sound: add zerolog

--- controller/middleware.go
+++ controller/middleware.go
@@ -3,13 +3,38 @@ package controller
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func LogError() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		start := time.Now()
+		path := ctx.Request.URL.Path
+		query := ctx.Request.URL.RawQuery
+		if query != "" {
+			path = path + "?" + query
+		}
 		ctx.Next()
+		errMsg := ctx.Errors.String()
+		statusCode := ctx.Writer.Status()
+		latency := time.Since(start)
+		method := ctx.Request.Method
+		clientIP := ctx.ClientIP()
+
+		var event *zerolog.Event
+		switch {
+		case statusCode >= 400 && statusCode < 500:
+			event = log.Warn()
+		case statusCode >= 500:
+			event = log.Error()
+		default:
+			event = log.Info()
+		}
+		event.Str("method", method).Str("path", path).Dur("response_time", latency).Int("status", statusCode).Str("client_ip", clientIP).Msg(errMsg)
 	}
 }
 
