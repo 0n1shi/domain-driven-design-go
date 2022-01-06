f6435879cd9364948a99731878a2ae35f80b3d98
Kazuki Onishi <41312202+0n1shi@users.noreply.github.com>
2022-01-06 22:08:04 +0900

    :truck: rename validate password function

--- domain/user/password.go
+++ domain/user/password.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"errors"
+	"unicode"
 )
 
 type UserPassword struct {
@@ -9,7 +10,7 @@ type UserPassword struct {
 }
 
 func NewUserPassword(password string) (*UserPassword, error) {
-	if !ValidatePassword(password) {
+	if !validatePassword(password) {
 		return nil, errors.New("at least 1 upper case, 1 lower case and numeric must be EMBEDDED somewhere in the middle of the password.")
 	}
 	return &UserPassword{val: password}, nil
@@ -17,4 +18,27 @@ func NewUserPassword(password string) (*UserPassword, error) {
 
 func (password *UserPassword) Get() string {
 	return password.val
+}
+
+func validatePassword(password string) bool {
+	length := false
+	upper := false
+	lower := false
+	numeric := false
+
+	if len(password) >= 8 {
+		length = true
+	}
+	for _, c := range password {
+		switch {
+		case unicode.IsNumber(c):
+			numeric = true
+		case unicode.IsUpper(c):
+			upper = true
+		case unicode.IsLower(c):
+			lower = true
+		}
+	}
+
+	return length && upper && lower && numeric
 }
